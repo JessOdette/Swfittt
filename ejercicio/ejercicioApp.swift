7abaf658215b608356294da226aff715159d2514
Jessica Odette Guizado Hernández <A01643724@tec.mx>
2023-12-05 17:22:59 -0600

    Initial Commit

---
+++ ejercicio/ejercicioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ejercicioApp.swift
+//  ejercicio
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ejercicioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
